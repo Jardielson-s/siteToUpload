68d913bcfdd161648a2f2636a0f45c133342046f
jardielson <jardielsonbio@gmail.com>
2021-07-25 12:04:47 -0300

    starting project

---
+++ site/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">    
+        <a className="App-link" href="https://reactjs.org">
+          Learn React
+        </a>
+        <h1> hello dev </h1>
+    </div>
+  );
+}
+
+export default App;
